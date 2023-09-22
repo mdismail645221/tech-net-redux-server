2186f014a1ca996e773fb1989b8b2f5c7c7d3a4c
Ismail Hossain <mdismail645221@gmail.com>
2023-09-22 19:04:13 +0600

    produt/:id roudte is all done

--- index.js
+++ index.js
@@ -1,51 +1,28 @@
+require('dotenv').config();
 const express = require('express');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
-const cors = require('cors');
-const port = 5000;
-require('dotenv').config()
-const jwt = require('jsonwebtoken');
+const port = process.env.PORT || 5000;
 
-// middleware
+const cors = require('cors');
+
 app.use(cors());
-// app.use(express.static("public"));
 app.use(express.json());
 
 
-app.get('/', (req, res)=> {
-    res.send('Hey, Your server successfully Run')
-})
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://tech-net:T1T0QpsTqZNRuxHf@cluster0.yzc4fwf.mongodb.net/?retryWrites=true&w=majority";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
-
-
 
 
 async function run() {
     try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
-
-
-
-      const techNetAllProducts = client.db("tech-net").collection("product")
-
-
-
-
+      const db = client.db('tech-net');
+      const techNetAllProducts = db.collection("product")
 
       app.get('/products', async(req, res)=> {
             const data = await techNetAllProducts.find({}).toArray();
@@ -54,32 +31,24 @@ async function run() {
 
       })
 
-
-
-
-
-
-
-
-
+      app.get('/product/:id', async (req, res) => {
+        const id = req.params.id;
+        const result = await techNetAllProducts.findOne({ _id: ObjectId.createFromHexString(id) });
+        console.log(result);
+        res.send(result);
+      });
 
     } catch (error) {
       // Ensures that the client will close when you finish/error
       console.log({error})
     }
   }
-  run().catch(console.dir);
+  run().catch(console.dir)
 
 
-
-
-
-
-
-
-
-
-
+  app.get('/', (req, res)=> {
+    res.send('Hey, Your server successfully Run')
+})
 
 app.listen(port, ()=> {
     console.log("tech net server is running", port, "Port")
